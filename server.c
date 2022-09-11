a7d53945c0b88917c9e4ee3126f804d16535d9c3
gaosong628 <51213408+gaosong628@users.noreply.github.com>
2022-09-11 15:43:57 -0500

    Update server.c

--- server.c
+++ server.c
@@ -21,7 +21,7 @@ int main(){
     perror("[-]Socket error");
     exit(1);
   }
-  printf("[+]TCP server socket created.\n");
+  //printf("[+]TCP server socket created.\n");
 
   memset(&server_addr, '\0', sizeof(server_addr));
   server_addr.sin_family = AF_INET;
@@ -33,7 +33,7 @@ int main(){
     perror("[-]Bind error");
     exit(1);
   }
-  printf("[+]Bind to the port number: %d\n", port);
+  //printf("[+]Bind to the port number: %d\n", port);
 
   listen(server_sock, 5);
   printf("Listening...\n");
@@ -41,13 +41,13 @@ int main(){
   {
       addr_size=sizeof(client_addr);
       client_sock=accept(server_sock, (struct sockaddr*)&client_addr, &addr_size);
-      printf("[+]Client connected.\n");
+      //printf("[+]Client connected.\n");
       
     while(1) {
       bzero(buffer,1024);
       recv(client_sock, buffer, sizeof(buffer),0);
       
-      printf("Message Received from Client: %s\n",buffer);
+      //printf("Message Received from Client: %s\n",buffer);
       send(client_sock,buffer,strlen(buffer),0);
     }
       close(client_sock);
